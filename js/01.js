34ebc91e035853ff1a23625faedebe7b63d1a445
valerivera10 <156733934+valerivera10@users.noreply.github.com>
2024-04-22 11:52:15 -0500

    first commit

---
+++ js/01.js
@@ -0,0 +1,43 @@
+let menu = "1. Ejercicio1\n2. Ejercicio2\n3. Ejercicio3\n4. Ejercicio4\n5. Ejercicio5\n6. Ejercicio6\n7. Ejercicio7\n8. Ejercicio8\n9.Ejercicio#9\n10. Ejercicio10\n11. Ejercicio11\n12. Ejercicio12\n13. Ejercicio13\n14. Ejercicio14\n15. Ejercicio15\n16. Ejercicio16\n17. Ejercicio17\n18. Salir"
+
+do{
+
+    opcion = parseInt(prompt(menu))
+    switch(opcion){
+        case 1:
+            pasword = prompt('Ingrese su contraseña')
+
+            function contrasenaValida (pasword){
+                if ((pasword === "2Fj(jjbFsuj") || (pasword === "eoZiugBf&g9")){
+                    return true
+                }
+                else {
+                    return false
+                }
+            }
+
+            console.log(contrasenaValida(pasword));
+            break;
+
+        case 2:
+            Edad = prompt('Ingrese la edad: ')
+            Ingresos = prompt('Ingrese los ingresos: ')
+            
+            function CalcularImpuestos (Edad, Ingresos) {
+                if ((Edad >= 18) && (Ingresos>=1000)) {
+                    console.log(Ingresos * 0.4)
+                    return 
+                }
+                else {
+                    return
+                }
+            }
+
+            console.log(CalcularImpuestos (Edad, Ingresos))
+            break;
+
+        case 3:
+            
+    }
+}while (opcion != 18)
+alert('Gracias')
